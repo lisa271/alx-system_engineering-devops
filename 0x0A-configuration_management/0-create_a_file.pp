ac12e954abaed6f713137d191aca07dd07706939
lisa271 <lizbmaloba@gmail.com>
2023-04-24 13:35:02 -0700

    create /tmp file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 #using puppet to create file in /tmp
 file { '/tmp/school':
-owner => "www-data",
-group => "www-data",
-mode => "0744",
-content => "I love puppet"
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => "I love Puppet"
 }
